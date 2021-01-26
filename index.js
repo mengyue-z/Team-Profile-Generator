e6d025faffca45c4146c90f1a588691a6fe2a36e
Mengyue Zhang <mengyue.z@outlook.com>
2021-01-25 23:33:19 -0500

    revised class files and updated addteam function

--- index.js
+++ index.js
@@ -1,36 +1,223 @@
 var inquirer = require('inquirer');
+var Manager = require('./lib/Manager');
+var Engineer = require('./lib/Engineer');
+var Intern = require('./lib/Intern');
 
-var questions = [
-    {
-        type: "input",
-        name: "manager",
-        message: "What is the team manager's name?"
-    },
-    {
-        type: "input",
-        name: "id",
-        message: "What is your employee ID?"
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "What is your email address?",
-        validate: function (email) {
-  
-            valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
-
-            if (valid) {
-                return true;
-            } else {
-                console.log("\n Please enter a valid email")
-                return false;
+function addManager() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "manager",
+            message: "What is the team manager's name?",
+            validate: answer => {
+                if (answer !=="") {
+                    return true;
+                } else {
+                    return "Please enter at least one character.";
+                }
             }
+        },
+        {
+            type: "input",
+            name: "managerId",
+            message: "What is Manager's employee ID?",
+            validate: answer => {
+                const pass= answer.match(
+                    /^[1-9]\d*$/
+                ); 
+                if (pass) {
+                    return true;
+                } else {
+                    return "Please enter a valid ID number greater than 0."
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "managerEmail",
+            message: "What is your email address?",
+            validate: function (email) {
+      
+                valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+    
+                if (valid) {
+                    return true;
+                } else {
+                    return "Please enter a valid email"
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "managerOfficeNumber",
+            message: "What is your office number?",
+            validate: answer => {
+                const pass= answer.match(
+                    /^[1-9]\d*$/
+                ); 
+                if (pass) {
+                    return true;
+                } else {
+                    return "Please enter a valid office number greater than 0."
+                }
+            },
+        },
+    ]).then((answers) => {
+        const manager = new Manager(answers.manager,answers.managerId,answers.managerEmail, answers.managerOfficeNumber);
+        console.log(manager);
+        addTeamMember();
+      });
+    };
+addManager();
+function addTeamMember() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'memberChoice',
+            message: 'Do you want to add someone in your team?',
+            choices: ['Engineer', 'Intern', 'finish building my team']
         }
-    }
+    ]).then(choice => {
+        switch (choice.memberChoice){
+            case "Engineer":
+                addEngineer();
+            break;
+            case "Intern":
+                addIntern();
+            break;
+            default:
+            console.log("Team profile completed!")
+        }
+    })
+};
 
-]
-inquirer.prompt(questions).then((answers) => {
-    console.log(answers.manager);
-    console.log(answers.id);
-    console.log(answers.email);
-  });
+function addEngineer() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "engineer",
+            message: "What is the Engineer's name?",
+            validate: answer => {
+                if (answer !=="") {
+                    return true;
+                } else {
+                    return "Please enter at least one character.";
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "engineerId",
+            message: "What is the Engineer's employee ID?",
+            validate: answer => {
+                const pass= answer.match(
+                    /^[1-9]\d*$/
+                ); 
+                if (pass) {
+                    return true;
+                } else {
+                    return "Please enter a valid ID number greater than 0."
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "engineerEmail",
+            message: "What is the Engineer's email address?",
+            validate: function (email) {
+      
+                valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+    
+                if (valid) {
+                    return true;
+                } else {
+                    return "Please enter a valid email"
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "engineerGithub",
+            message: "What is the Engineer's Github username?",
+            validate: answer => {
+                if (answer !=="") {
+                    return true;
+                } else {
+                    return "Please enter at least one character.";
+                }
+            }
+        },
+    
+    ]).then(answers => {
+        const engineer = new Engineer(answers.engineer,answers.engineerId,answers.engineerEmail, answers.engineerGithub);
+        console.log(engineer);
+        addTeamMember();
+    });
+};
+
+
+function addIntern(){
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "intern",
+            message: "What is the Intern's name?",
+            validate: answer => {
+                if (answer !=="") {
+                    return true;
+                } else {
+                    return "Please enter at least one character.";
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "internId",
+            message: "What is the Intern's employee ID?",
+            validate: answer => {
+                const pass= answer.match(
+                    /^[1-9]\d*$/
+                ); 
+                if (pass) {
+                    return true;
+                } else {
+                    return "Please enter a valid ID number greater than 0."
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "internEmail",
+            message: "What is the Intern's email address?",
+            validate: function (email) {
+      
+                valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+    
+                if (valid) {
+                    return true;
+                } else {
+                    return "Please enter a valid email"
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "school",
+            message: "What is the Intern's school?",
+            validate: answer => {
+                if (answer !=="") {
+                    return true;
+                } else {
+                    return "Please enter at least one character.";
+                }
+            }
+        },
+    ]).then(answers => {
+        const intern = new Intern(answers.intern, answers.internId, answers.internEmail, answers.school);
+        console.log(intern);
+        addTeamMember();
+    })
+} 
+
+
+
